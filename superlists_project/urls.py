ee646dc5da1d4c2b17c696c5ba1244356d598019
MaksimMarynau <oliverknightbel@gmail.com>
2022-02-05 12:04:45 +0100

    new URL, view and template to display lists

--- superlists_project/urls.py
+++ superlists_project/urls.py
@@ -21,5 +21,6 @@ from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/unique_list', views.view_list, name='view_list'),
     # url(r'^admin/', admin.site.urls),
 ]
