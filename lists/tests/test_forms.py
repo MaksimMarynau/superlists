dfe93d90f6c6904a24e34d0bca7d5502391cf6c0
MaksimMarynau <oliverknightbel@gmail.com>
2022-02-27 18:35:20 +0100

    using existing list item form in list view

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -20,10 +20,7 @@ class ItemFormTest(TestCase):
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            [EMPTY_ITEM_ERROR]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
     def test_form_save_handles_saving_to_a_list(self):
         list_ = List.objects.create()
@@ -44,17 +41,17 @@ class ExistingListItemFormTest(TestCase):
         list_ = List.objects.create()
         form = ExistingListItemForm(for_list=list_, data={'text': ''})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            [EMPTY_ITEM_ERROR]
-        )
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
 
     def test_form_validation_for_duplicate_items(self):
         list_ = List.objects.create()
         Item.objects.create(list=list_, text='no twins!')
-        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins'})
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
         self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.errors['text'],
-            [DUPLICATE_ITEM_ERROR]
-        )
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
+
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
